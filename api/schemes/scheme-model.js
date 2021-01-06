3fafbee2a8e241817b9ff90e091ef731779959df
KamrynR <62856359+KamrynR@users.noreply.github.com>
2021-01-06 00:10:37 -0600

    mvp

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,13 @@
-// scheme-model
+const db = require('../../data/db-config')
+
+function findStepsForScheme(id) {
+    return db('steps')
+        .innerJoin('schemes', 'schemes.id', 'steps.scheme_id')
+        .where('scheme_id', id)
+        .select('steps.id', 'step_number', 'steps.instructions', 'schemes.scheme_name')
+        .orderBy('step_number', 'asc')
+}
+
+module.exports = {
+    findStepsForScheme,
+}
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const server = require('./api/server.js');
+const express = require('express')
+const schemeRouter = require('./api/schemes/scheme-router')
 
-const PORT = process.env.PORT || 5000;
+const server = express()
+const PORT = process.env.PORT || 8080
+
+server.use(express.json());
+server.use('/api/schemes', schemeRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+  console.log(`Listening on port ${PORT}...`)
+})
